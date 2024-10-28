988ec01113cf406f77f39f8c4b0811941d90907f
rdnfn <75615911+rdnfn@users.noreply.github.com>
2024-10-28 09:55:26 +0000

    update app info texts

--- src/inverse_cai/app/info_texts.py
+++ src/inverse_cai/app/info_texts.py
@@ -9,8 +9,15 @@ METHOD_INFO_TEXT = """
 Many ML researchers use pairwise feedback as an optimization objective, both for training or evaluation. However, few really understand *what implicit goals* they are training (or evaluating) their models towards, other than "aligning with human values". ICAI helps to understand these implicit goals by compressing large amounts of pairwise feedback data into individual principles, each a possible explanation of annotators' decisions.
 
 **Method.**
-Our initial *Inverse Constitutional AI* (ICAI) implementation consists of two core steps: (A) *proposing hypothesis principles* that *might* explain annotators' decisions, and (B) *evaluating each principle* by how well an LLM following it is able to reconstruct the original pairwise feedback data. Below you can see the principles with the corresponding reconstruction performance metrics. See the [paper](https://arxiv.org/abs/2406.06560) for full details on the ICAI method.
+Our initial *Inverse Constitutional AI* (ICAI) implementation consists of two core steps: (A) *proposing hypothesis principles* that *might* explain annotators' decisions, and (B) *evaluating each principle* by how well an LLM following it is able to reconstruct the original pairwise feedback data. Below you can see the principles with the corresponding reconstruction performance metrics. See the [paper](https://arxiv.org/abs/2406.06560) or the [code](https://github.com/rdnfn/icai) for full details on the ICAI method.
 
 **Interpretation of results.**
 A principle that performs (i.e. reconstructs the feedback) well might be a good explanation of the annotators' reasoning process – or at least correlates with that process. Using the corresponding feedback for training may lead to models that follow these principles. For example, if the principle *"select the more concise response"* reconstructs a feedback dataset well, a model trained on this dataset may become more concise. Similarly, in the context of evaluation, if such a principle shows up in feedback that prefers a certain model, then this model likely follows this principle.
+
+**Sources of variability.**
+There are many different reasons why one set of annotator or annotations leads to different rules reconstructing it well:
+1. *Contradicting annotator preferences:* the underlying annotator preferences are truly different and contradicting, and thus principles achieve different performance on each dataset.
+2. *Different (possibly non-contradicting) revealed preferences:* in many datasets (e.g. PRISM, Chatbot Arena) the annotators choose themselves what to talk about with the AI. Whilst such annotators may overall agree (e.g., they would label each other's interactions similarly), revealed preference may still reveal something about what principles are more important to them for their use-cases --- even if their preferences are not contradictory.
+3. *Noisy LLM reconstructions:* Some variability may also be explained by the underlying LLM not consistently using one rule in a certain way. Whilst some principles may be clearer to interpret, others can be more vague (and thus lead to noisy reconstructions). Running ICAI on larger datasets should mitigate this effect to certain degree.
+
 """
