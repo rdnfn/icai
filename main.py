75327cae437f44042e94a1a9b38f90a58faa0dd6
rdnfn <75615911+rdnfn@users.noreply.github.com>
2024-11-04 10:26:04 +0000

    simplify main py script

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import gradio.themes.utils.fonts
-
-import inverse_cai.app.interface as interface
-
-# make gradio work offline
-gradio.themes.utils.fonts.GoogleFont.stylesheet = lambda self: None
-
-
-demo = interface.generate()
-
-
-def run():
-
-    demo.launch()
-
+from inverse_cai.app.main import run
 
 if __name__ == "__main__":
     run()  # pylint: disable=no-value-for-parameter
