0194175df716ff983c1c86e61241531000c5395c
rdnfn <75615911+rdnfn@users.noreply.github.com>
2025-06-08 11:34:39 +0100

    Remove unused partial implementation of cross prompt principle generation

--- src/inverse_cai/algorithm/proposal.py
+++ src/inverse_cai/algorithm/proposal.py
@@ -91,51 +91,7 @@ def generate_principles_from_feedback(
             feedback.at[index, "prompt_principles"] = prompt_principles
 
     elif num_rankings_per_sampling_step > 1:
-
         raise NotImplementedError
-
-        # allocate group_ids, such that each row is assigned to a random equally
-        # sized group
-        n_groups = -(len(feedback) // -num_rankings_per_sampling_step)
-        feedback["group_id"] = pd.qcut(
-            np.random.permutation(len(feedback)), q=n_groups, labels=False
-        )
-        assert (
-            feedback["group_id"].value_counts().max() <= num_rankings_per_sampling_step
-        ), f"Number of rankings per group must be less than or equal to {num_rankings_per_sampling_step}, max is {feedback['group_id'].value_counts().max()}"
-        assert (
-            feedback["group_id"].unique().shape[0] == n_groups,
-            f"Number of groups ({feedback['group_id'].unique().shape[0]}) must be equal to {n_groups}",
-        )
-
-        def process_multiple_rows(
-            index, rows, num_principles_per_sampling_step, model_name, config
-        ):
-            principles = generate_principles_from_multiple_rankings(
-                preferred_texts=[get_preferred_text(row) for _, row in rows.iterrows()],
-                rejected_texts=[get_rejected_text(row) for _, row in rows.iterrows()],
-                num_principles=num_principles_per_sampling_step,
-                model_name=model_name,
-                config=config,
-            )
-            return index, principles
-
-        # parallelize the process
-        results = Parallel(n_jobs=config.parallel_workers)(
-            delayed(process_multiple_rows)(
-                index, rows, num_principles_per_sampling_step, model_name, config
-            )
-            for index, rows in tqdm.tqdm(
-                feedback.groupby("group_id"), total=feedback.shape[0]
-            )
-        )
-
-        # update the feedback DataFrame, adding groups principles to each row in group
-        for index, principles in results:
-            # add principles to each row in the group
-            feedback.loc[feedback["group_id"] == index]["principles"] = [
-                principles
-            ] * len(feedback[feedback["group_id"] == index])
 
     # get list of all principles (note that in results
     # principles are lists of principles)
@@ -255,75 +211,6 @@ def generate_principles_from_single_ranking(
             logger.error(e)
 
     return principles, prompt_principles
-
-
-def generate_principles_from_multiple_rankings(
-    preferred_texts: list[str],
-    rejected_texts: list[str],
-    num_principles: int,
-    model_name: str,
-    config: ExpConfig,
-) -> list:
-    """
-    Generate principles from multiple rankings.
-
-    Args:
-        preferred_texts: List of preferred texts.
-        rejected_texts: List of rejected texts.
-        num_principles: The number of principles to generate.
-        model_name: The name of the model to use.
-        config: The experiment configuration.
-
-    Returns:
-        A list of principles.
-    """
-    raise NotImplementedError
-    assert num_principles > 0, "Number of principles must be greater than 0"
-    assert len(preferred_texts) == len(
-        rejected_texts
-    ), "Number of preferred and rejected texts must match"
-    assert len(preferred_texts) > 0, "At least one ranking must be provided"
-
-    # get the model
-    model = inverse_cai.models.get_model(model_name)
-    principles: list = []
-
-    rankings_str = "\n".join(
-        [
-            f"## Ranking {i+1}:\n### Preferred: {preferred_text}\n\n### Rejected: {rejected_text}\n\n-----\n\n"
-            for i, (preferred_text, rejected_text) in enumerate(
-                zip(preferred_texts, rejected_texts)
-            )
-        ]
-    )
-
-    for prompt in config.alg_prompts.generator_prompts:
-        messages = inverse_cai.algorithm.utils.parse_prompt(
-            prompt_str=prompt,
-            prompt_kwargs=dict(
-                rankings=rankings_str,
-                num_principles=num_principles,
-            ),
-        )
-
-        # generate principles
-        principle_output = model.invoke(messages).content
-
-        # parse the principles
-        try:
-            principle_output = clean_principle_str(principle_output)
-            parsed_output = ast.literal_eval(principle_output)["principles"]
-            principles += parsed_output
-            if len(parsed_output) < num_principles:
-                logger.warning(
-                    f"Generated fewer ({len(parsed_output)}) principles "
-                    f"than expected ({num_principles})"
-                )
-        except Exception as e:
-            logger.error(f"Failed to parse principles: {principle_output}")
-            logger.error(e)
-
-    return principles
 
 
 def clean_principle_str(principle_str: str) -> str:
