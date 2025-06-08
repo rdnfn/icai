9571ceaa7be21c5c820c494c996e4df833ae521a
rdnfn <75615911+rdnfn@users.noreply.github.com>
2025-06-08 10:57:36 +0100

    Remove API call logger
    As no longer functioning with calls across multiple processes

--- src/inverse_cai/models.py
+++ src/inverse_cai/models.py
@@ -6,74 +6,10 @@ import os
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_community.callbacks import get_openai_callback
-import inverse_cai.local_secrets
+import inverse_cai.local_secrets  # required to load env vars
 from loguru import logger
 
 NULL_LOGPROB_VALUE = -1000000
-
-
-class LogWrapper:
-    def __init__(self, wrapped_instance):
-        """Initialize the wrapper with the instance to wrap."""
-        self._wrapped_instance = wrapped_instance
-        self._logger = logger
-
-        if hasattr(wrapped_instance, "model_name"):
-            self.model_name = wrapped_instance.model_name
-        elif hasattr(wrapped_instance, "model"):
-            self.model_name = wrapped_instance.model
-
-    def __getattr__(self, attr):
-        """
-        Transparently handle method calls and property access.
-        """
-        original_attr = getattr(self._wrapped_instance, attr)
-        if callable(original_attr):
-
-            def hooked(*args, **kwargs):
-                # Before calling the original method
-                self._logger.log(
-                    "LM_API_CALL",
-                    f'{{"type":"input","class_method":"{attr}","args":{json.dumps(str(args))}, "kwargs":{json.dumps(str(kwargs))}}}, "model": "{self.model_name}", "model_class": "{self._wrapped_instance.__class__.__name__}", "model_class_str": "{str(self._wrapped_instance)}"}}',
-                )
-
-                # check if openai model used,
-                # then add token_tracking
-                if "openai" in self._wrapped_instance.__class__.__name__.lower():
-                    get_callback = get_openai_callback
-                else:
-                    get_callback = None
-
-                if get_callback is not None:
-                    with get_callback() as callback:
-                        # Call the original method
-                        result = original_attr(*args, **kwargs)
-                else:
-                    result = original_attr(*args, **kwargs)
-
-                if get_callback is not None:
-                    token_usage = json.dumps(
-                        {
-                            "str": json.dumps(str(callback)),
-                            "cost": callback.total_cost,
-                            "tokens": callback.total_tokens,
-                            "prompt_tokens": callback.prompt_tokens,
-                            "completion_tokens": callback.completion_tokens,
-                        }
-                    )
-                else:
-                    token_usage = '"(not available)"'
-
-                # After calling the original method
-                self._logger.log(
-                    "LM_API_CALL",
-                    f'{{"type":"return_value","class_method":"{attr}", "value": {json.dumps(str(result))}, "model": "{self.model_name}", "model_class": "{self._wrapped_instance.__class__.__name__}", "model_class_str": {json.dumps(str(self._wrapped_instance))}, "token_usage": {token_usage}}}',
-                )
-                return result
-
-            return hooked
-        else:
-            return original_attr
 
 
 def get_model(
@@ -99,23 +35,21 @@ def get_model(
         model_kwargs = {}
 
     if name.startswith("openai"):
-        return LogWrapper(
-            ChatOpenAI(
-                model=name.split("/")[1],
-                max_tokens=max_tokens,
-                temperature=temp,
-                model_kwargs=model_kwargs,
-            )
+        return ChatOpenAI(
+            model=name.split("/")[1],
+            max_tokens=max_tokens,
+            temperature=temp,
+            model_kwargs=model_kwargs,
         )
+
     if name.startswith("anthropic"):
-        return LogWrapper(
-            ChatAnthropic(
-                model=name.split("/")[1],
-                max_tokens=max_tokens,
-                temperature=temp,
-                model_kwargs=model_kwargs,
-            )
+        return ChatAnthropic(
+            model=name.split("/")[1],
+            max_tokens=max_tokens,
+            temperature=temp,
+            model_kwargs=model_kwargs,
         )
+
     if name.startswith("openrouter"):
         # Custom OpenRouter headers
         # https://openrouter.ai/docs/api-reference/overview#headers
@@ -144,16 +78,14 @@ def get_model(
                 "OPENROUTER_API_KEY environment variable must be set for OpenRouter models"
             )
 
-        return LogWrapper(
-            ChatOpenAI(
-                model=model_id,
-                max_tokens=max_tokens,
-                temperature=temp,
-                openai_api_key=openrouter_api_key,  # Use the OpenRouter API key
-                openai_api_base="https://openrouter.ai/api/v1",
-                default_headers=custom_headers,
-                model_kwargs=model_kwargs,
-            )
+        return ChatOpenAI(
+            model=model_id,
+            max_tokens=max_tokens,
+            temperature=temp,
+            openai_api_key=openrouter_api_key,  # Use the OpenRouter API key
+            openai_api_base="https://openrouter.ai/api/v1",
+            default_headers=custom_headers,
+            model_kwargs=model_kwargs,
         )
 
 
